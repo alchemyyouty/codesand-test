d3495d0e0f3c0a0ed0cd716e5d5e1acd6c93ef3b
齋藤 雄太 <51504981+alchemyyouty@users.noreply.github.com>
2022-01-24 11:00:33 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import "./styles.css";
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 > 0 ? "trueです" : "falseです";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください。'
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を越えています！' : '許容範囲内です';
+// }
+// console.log(checkSum(50, 10));
